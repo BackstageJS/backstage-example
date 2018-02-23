450695a978ef9fa909b137de2cb41124beddafd7
Jesse Pinho <jesse@jessepinho.com>
2018-02-23 21:11:53 +0100

    fix: pass a config object to backstage()

--- src/app.js
+++ src/app.js
@@ -2,4 +2,7 @@ const path = require('path')
 const { backstage } = require('backstage-server')
 const { fileSystem } = require('backstage-server/dist/storage-backends/file-system')
 
-backstage(fileSystem(path.join(__dirname, '../files'))).listen(3000)
+const fileDir = path.join(__dirname, '../files')
+const storageBackend = fileSystem(fileDir)
+
+backstage({ storageBackend }).listen(3000)
