61e99069faf65dc47e7b94ec913e1e86938d90eb
Jesse Pinho <jesse@jessepinho.com>
2022-03-17 15:27:23 -0700

    Committing uncommitteed changes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const path = require('path')
 const { backstage } = require('backstage-server')
 const { fileSystem } = require('backstage-server/dist/storage-backends/file-system')
+const { cookie } = require('backstage-server/dist/package-resolvers/cookie')
 
 const fileDir = path.join(__dirname, '../files')
 const storageBackend = fileSystem(fileDir)
 
-backstage({ storageBackend }).listen(3000)
+backstage({ packageResolver: cookie, storageBackend }).listen(3000)
